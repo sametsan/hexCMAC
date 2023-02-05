3f651c7a77a68595575e958d84f766211f7ea8c8
Samet SAN <sametsan88@gmail.com>
2023-02-06 00:27:55 +0300

    release 0.1

--- main.c
+++ main.c
@@ -21,6 +21,7 @@ typedef struct{
 void print_hex(uint8_t *data,uint16_t len){
     for(uint16_t i=0;i<len;i++)
         printf("%x",data[i]);
+    printf("\n");
 }
 
 uint8_t char2hex(char c){
@@ -69,7 +70,7 @@ bool save_binary_file(const char *filename,uint8_t *buffer,uint32_t buffer_len){
     return true;
 }
 
-bool load_hex_file(const char *filename,uint8_t *buffer,uint32_t *size,uint16_t startAddr,uint16_t endAddr){
+bool load_hex_file(const char *filename,uint8_t *buffer,uint16_t *size,uint16_t startAddr,uint16_t endAddr){
     uint16_t disk_size = endAddr - startAddr;
     uint8_t disk[disk_size];
     memset(disk,0xFF,disk_size);
@@ -84,7 +85,7 @@ bool load_hex_file(const char *filename,uint8_t *buffer,uint32_t *size,uint16_t 
     while (!feof(fp))
     {
         if(getLine(fp,&line)){
-            printf("0x%08x  ",line.address,line.size);
+            printf("0x%08x  ",line.address);
             for(uint8_t i=0;i<line.size;i++)
                 printf("0x%02x ",line.data[i]);
 
@@ -104,8 +105,20 @@ bool load_hex_file(const char *filename,uint8_t *buffer,uint32_t *size,uint16_t 
 }
 
 bool load_key_file(const char *filename,uint8_t *buffer){
-    uint8_t key[]={0x95,0xff,0x63,0x85,0x16,0x72,0x54,0xf9,0xa1,0x20,0x11,0x23,0xab,0xc4,0xc5,0x8a};
-    memcpy(buffer,key,16);
+
+    FILE *fp = fopen(filename,"rb");
+    if(fp == NULL)
+        return false;
+
+    for(uint8_t i=0;i<16;i++){
+        buffer[i] = (NEXT_BYTE << 4) | NEXT_BYTE;
+    }
+
+    //fread(buffer,1,16,fp);
+    fclose(fp);
+
+    printf("Loaded key : ");
+    print_hex(buffer,16);
 }
 
 bool generate_cmac(uint8_t *data,uint16_t data_len,uint8_t *key,uint8_t *cmac){
@@ -114,10 +127,10 @@ bool generate_cmac(uint8_t *data,uint16_t data_len,uint8_t *key,uint8_t *cmac){
 
     CMAC_CTX *verifyCMAC = CMAC_CTX_new();
 
-    CMAC_Init(verifyCMAC, key,16, EVP_aes_256_cbc(), NULL);
+    CMAC_Init(verifyCMAC, key,16, EVP_aes_128_cbc(), NULL);
 
     CMAC_Update(verifyCMAC,data,data_len);
-    CMAC_Final(verifyCMAC, cmac,&cmac_size);
+    CMAC_Final(verifyCMAC, cmac,(size_t*)&cmac_size);
 
     CMAC_CTX_free(verifyCMAC);
 
@@ -126,7 +139,7 @@ bool generate_cmac(uint8_t *data,uint16_t data_len,uint8_t *key,uint8_t *cmac){
 int main(int argc,char *argv[]){
 
     if(argc < 5 ){
-        printf("hexCMAC <aesKey> <inputfile> <startaddress> <endaddress>\n");
+        printf("hexCMAC <aesKeyFile> <hexFile> <startAddress> <endAddress>\n");
         return 1;
     }
 
@@ -143,11 +156,13 @@ int main(int argc,char *argv[]){
 
     save_binary_file("./binary.bin",disk,size);
 
+    printf("Disk : ");
+    print_hex(disk,size);
+
     generate_cmac(disk,size,key,cmac);
 
-    printf("CMAC : ");
+    printf("Signature : ");
     print_hex(cmac,16);
-    NEW_LINE;
 
     return 0;
 }
